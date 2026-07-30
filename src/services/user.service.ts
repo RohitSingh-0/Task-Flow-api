3a60b349dc82355084fb149288b6de2b9e4b5da2
Rohit <0rohitrajput00@gmail.com>
2026-07-30 14:59:21 +0530

    feat: implement password hashing and improve validation handling

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,46 +2,49 @@ import { userRepository } from "../repositories/user.repository.js";
 import type { UserType } from "../types/user.types.js";
 import { isPasswordValid, isValidEmail } from "../utilities/validation.js";
 import { AppError } from "../errors/AppError.js";
+import bcrypt from "bcrypt";
 
 export const userService = {
     async createUser(userData: UserType): Promise<UserType> {
 
         if (!userData.name) {
-            throw new Error("name is required");
+            throw new AppError("Name is required", 400);
         }
         if (!userData.email) {
-            throw new Error("email is required")
+            throw new AppError("Email is required", 400)
         }
         if (!userData.password) {
-            throw new Error("Password is required.")
+            throw new AppError("Password is required.", 400)
         }
-        
+
         if (!isValidEmail(userData.email)) {
-            throw new Error("Invalid Email Format")
+            throw new AppError("Invalid Email Format", 400)
         }
         if (!isPasswordValid(userData.password)) {
-            throw new Error("Password must contain at least 8 characters, one uppercase letter, one lowercase letter,one number and one special character")
+            throw new AppError("Password must contain at least 8 characters, one uppercase letter, one lowercase letter,one number and one special character", 400)
         }
 
         if (!userData.role) {
-            throw new Error("Role is required")
+            throw new AppError("Role is required", 400)
         }
 
         const validRoles = ["Admin", "Manager", "Employee"];
         if (!validRoles.includes(userData.role)) {
-            throw new Error("Role must be Admin, Manager or Employee");
+            throw new AppError("Role must be Admin, Manager or Employee", 400);
         }
 
 
         if (userData.isActive === undefined) {
-            throw new Error("isActive is required")
+            throw new AppError("isActive is required", 400)
         }
 
         const isUserExist = await userRepository.findByEmail(userData.email)
-        
+
         if (isUserExist) {
             throw new AppError("User Already Exists", 409);
         }
+        const hashedPassword = await bcrypt.hash(userData.password, 10)
+        userData.password = hashedPassword;
 
         const userCreated = await userRepository.createUser(userData);
         return userCreated;
